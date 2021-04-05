94dc83b353963504b20ba2975682e7bc04c49fbf
Patrick Van den Bergh <patrick@ansapa.com>
2021-04-05 09:26:31 +0200

    Initial Commit

---
+++ MarkdownEditor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarkdownEditor
+//
+//  Created by Patrick Van den Bergh on 05/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarkdownEditor/MarkdownEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownEditorApp.swift
+//  MarkdownEditor
+//
+//  Created by Patrick Van den Bergh on 05/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
