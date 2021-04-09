d09749ad7440afab12552663f4610693767cc3f5
Patrick Van den Bergh <patrick@ansapa.com>
2021-04-09 11:43:58 +0200

    Added unit testing.

---
+++ MarkdownEditorTests/MarkdownEditorTests.swift
@@ -0,0 +1,76 @@
+//
+//  MarkdownEditorTests.swift
+//  MarkdownEditorTests
+//
+//  Created by Patrick Van den Bergh on 09/04/2021.
+//
+
+import XCTest
+
+class MarkdownEditorTests: XCTestCase {
+    // Test cases are stored in Tests.json.
+    // This file is build on the CommonMark Spec v0.29 (2019-04-06)
+    // see https://spec.commonmark.org/0.29/
+
+    struct Test: Decodable {
+        let markdown: String
+        let html: String
+        let example: Int
+        let start_line: Int
+        let end_line: Int
+        let section: String
+    }
+    
+    struct Score {
+        var success: Int
+        var total: Int
+    }
+
+    var tests = [Test]()
+    var score = Score(success: 0, total: 0)
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        var contents = ""
+        let bundle = Bundle(for: type(of: self))
+        if let filepath = bundle.path(forResource: "Tests", ofType: "json") {
+            do {
+                contents = try String(contentsOfFile: filepath)
+            } catch {
+                fatalError("Can not read info.plist")
+            }
+        }
+        let jsonData = contents.data(using: .utf8)!
+        tests = try! JSONDecoder().decode([Test].self, from: jsonData)
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testCommonMarkSpec() throws {
+        for test in tests {
+            print("============")
+            print("testCase \(test.example)")
+            print("============")
+            let markdown = test.markdown
+            print("Markdown:\n\(markdown)")
+            let html = test.html
+            print("Expected HTML:\n\(html)")
+            let generatedHtml = Scanner(source: markdown).getHtml()
+            print("Generated HTML:\n\(generatedHtml)")
+            if generatedHtml ==  html {
+                score.success += 1
+            }
+            score.total += 1
+            XCTAssertTrue(generatedHtml == html)
+        }
+        print("+=======+")
+        print("| Score |")
+        print("+=======+")
+        print()
+        print("\(score.success) / \(score.total)")
+        print()
+        print()
+    }
+}
